9e0bb18fe41ba95c75bc2306eb39dbd381539832
Kaylin Burns <112958860+SeranaZ@users.noreply.github.com>
2024-11-17 14:15:41 -0500

    Update MakeVehicle.sh

--- missions/s1_alpha/MakeVehicle.sh
+++ missions/s1_alpha/MakeVehicle.sh
@@ -23,9 +23,10 @@ n=9000
 m=0
 # Run through every set of vehicles by dividing the line count
 # by the number of variables required for each nsplug call
-for i in $(seq 1 $((line_count / 8))); do
+for i in $(seq 1 $((line_count / 9))); do
     nsplug vehicle.moos "$vehicle_$m.moos" \
     PORT="$n" \
+    VNAME="$vehicle_$m" \
     LATORG="${my_array[$((1 + next_vehicle))]}" \
     LONGORG="${my_array[$((2 + next_vehicle))]}" \
     START_X="${my_array[$((3 + next_vehicle))]}" \
@@ -35,7 +36,7 @@ for i in $(seq 1 $((line_count / 8))); do
     VEHICLE_TYPE="${my_array[$((7 + next_vehicle))]}" \
     VEHICLE_COLOR="${my_array[$((8 + next_vehicle))]}" \
   # Increment next_vehicle by 4 to move to the next set of variables
-  ((next_vehicle += 8)) #we divide by 9 because of the number of
+  ((next_vehicle += 9)) #we divide by 9 because of the number of
   #nsplug variables minus the port, we need to divide to move
   #to the next line for the next vehicle set, so if you tinker nsplug
   #to add another tweakable variable to the vehicle default file PLEASE increment
