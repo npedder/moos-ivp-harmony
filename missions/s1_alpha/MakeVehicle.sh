d4975b02400e3028c027f3a9748649549199de74
Kaylin Burns <112958860+SeranaZ@users.noreply.github.com>
2024-11-23 17:29:38 -0500

    Made changes to MakeVehicle, launch, and Init file
    removed bugs from afformentioned files and file dependencies

---
+++ missions/s1_alpha/MakeVehicle.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+#remove vehicle files and rebuild so if changes are made
+#or a vehicle is removed singularly because config
+#changes, then you have a safe bet nothing will
+#be misalligned of broken in a file
+#!/bin/bash
+
+word="vehicle_"
+
+# Check for file names containing the word
+if ls *"$word"* 1> /dev/null 2>&1; then
+rm *vehicle_*
+fi
+
+# Define the file
+file="InitVehicleConfig.txt"
+
+# Count the number of lines in the file
+line_count=$(wc -l < "$file")
+
+# Declare array to store line data
+my_array=()
+
+# Fill array with line data to input into each .moos variable
+for i in $(seq 1 "$line_count"); do
+  LINE_DATA=$(sed -n "${i}p" "$file")
+  my_array[$((i - 1))]="$LINE_DATA"  # Store in zero-based index
+done
+
+# Counter to track the next vehicle's variables
+next_vehicle=0
+
+# Port number to increment
+n=9000
+m=0
+# Run through every set of vehicles by dividing the line count
+# by the number of variables required for each nsplug call
+for i in $(seq 1 $((line_count / 11))); do
+    nsplug vehicle.moos vehicle_"$m.moos" \
+    PORT="$n" \
+    VNAME=vehicle_"$m" \
+    LATORG="${my_array[$((0 + next_vehicle))]}" \
+    LONGORG="${my_array[$((1 + next_vehicle))]}" \
+    START_X="${my_array[$((2 + next_vehicle))]}" \
+    START_Y="${my_array[$((3 + next_vehicle))]}" \
+    HEADING="${my_array[$((4 + next_vehicle))]}" \
+    SPEED="${my_array[$((5 + next_vehicle))]}" \
+    VEHICLE_TYPE="${my_array[$((6 + next_vehicle))]}" \
+    VEHICLE_COLOR="${my_array[$((7 + next_vehicle))]}" \
+    NAME="${my_array[$((8 + next_vehicle))]}" \
+    ENDURANCE="${my_array[$((9 + next_vehicle))]}" \
+    SENSOR_RANGE="${my_array[$((10 + next_vehicle))]}" \
+
+
+  # Increment next_vehicle by 4 to move to the next set of variables
+  ((next_vehicle += 11)) #we divide by 8 because of the number of
+  #nsplug variables minus the port, we need to divide to move
+  #to the next line for the next vehicle set, so if you tinker nsplug
+  #to add another tweakable variable to the vehicle default file PLEASE increment
+  # to divide by your new amount of variables AND add
+  # the variable here for easy execution!
+
+#increment port number
+((n++))
+((m++))
+#if [[ "$VNAME" || "$VEHCLE_TYPE" || "$VEHCLE_COLOR"  =~ ^-?[0-9]+$ ]]; then
+    #echo "$One of your elements is of incorrect type, please check the #variable.txt script"
+#elif  [["$LATORG" || "$LONGORG" || "$START_X" || "$START_Y" || "$HEADING" || "$SPEED" =~ ^[a-zA-Z]+$ ]]; then
+    #echo "$One of your elements is of incorrect type, please check the #variable.txt script"
+#fi
+done
+#check if size of array is less than value for testing purposes, return error and that code wont work
+#also add line_count - 1 to live vehicle.txt vehicle instructions without reading the last line of code
---
+++ missions/s1_alpha/launch.sh
@@ -0,0 +1,72 @@
+#!/bin/bash -e
+#----------------------------------------------------------
+#  Script: launch.sh
+#  Author: Michael Benjamin
+#  LastEd: May 20th 2019
+#----------------------------------------------------------
+#  Part 1: Set Exit actions and declare global var defaults
+#----------------------------------------------------------
+
+TIME_WARP=1
+COMMUNITY="vehicle_0"
+GUI="yes"
+m=0
+n=0
+#----------------------------------------------------------
+#  Part 2: Check for and handle command-line arguments
+#----------------------------------------------------------
+for ARGI; do
+    if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ] ; then
+        echo "launch.sh [SWITCHES] [time_warp]   "
+        echo "  --help, -h           Show this help message            " 
+        exit 0;
+    elif [ "${ARGI}" = "--nogui" ] ; then
+        GUI="no"
+    elif [ "${ARGI//[^0-9]/}" = "$ARGI" -a "$TIME_WARP" = 1 ]; then 
+        TIME_WARP=$ARGI
+    else 
+        echo "launch.sh Bad arg:" $ARGI " Exiting with code: 1"
+        exit 1
+    fi
+done
+
+
+#----------------------------------------------------------
+#  Part 3: Launch the processes
+#----------------------------------------------------------
+
+# Find all vehicle_* files in the current directory
+vehicle_files=(vehicle_*.moos)
+
+# Get the total number of vehicle files found
+m=${#vehicle_files[@]}
+
+if [ $m -eq 0 ]; then
+    echo "No vehicle_*.moos files found. Exiting."
+    exit 1
+fi
+
+echo "Launching $COMMUNITY MOOS Community with WARP:" $TIME_WARP
+
+# Launch the main community process
+pAntler $COMMUNITY.moos --MOOSTimeWarp=$TIME_WARP >& /dev/null &
+
+# Launch all vehicle_*.moos files
+for vehicle in "${vehicle_files[@]}"; do
+    pAntler $vehicle --MOOSTimeWarp=$TIME_WARP >& /dev/null &
+done
+
+# Launch the shoreside process
+pAntler shoreside.moos --MOOSTimeWarp=$TIME_WARP >& /dev/null &
+
+# Launch uMAC for shoreside and all vehicles
+uMAC -t shoreside.moos &
+for vehicle in "${vehicle_files[@]}"; do
+    uMAC -t $vehicle &
+done
+
+# Launch uMAC for the main community
+uMAC -t $COMMUNITY.moos
+
+# Kill all processes after completion
+kill -- -$$
