79dfdaf8dadfb2b0bf2fc1ae28a908a0ef6670c7
SeranaZ <seranazentha@gmail.com>
2024-11-23 17:11:27 -0500

    edited files in order to remove file dependencies in order to run and fix bugs

--- launch.sh
+++ launch.sh
@@ -6,22 +6,22 @@
 #----------------------------------------------------------
 #  Part 1: Set Exit actions and declare global var defaults
 #----------------------------------------------------------
-#source file to run your congfig and vehicle amounts before launch!
-m=(grep'^m='MakeVehicle.sh|cut -d'='-f2|tr-d"")
+
 TIME_WARP=1
 COMMUNITY="vehicle_0"
 GUI="yes"
+m=0
 n=0
 #----------------------------------------------------------
 #  Part 2: Check for and handle command-line arguments
 #----------------------------------------------------------
 for ARGI; do
     if [ "${ARGI}" = "--help" -o "${ARGI}" = "-h" ] ; then
-	echo "launch.sh [SWITCHES] [time_warp]   "
-	echo "  --help, -h           Show this help message            " 
-	exit 0;
+        echo "launch.sh [SWITCHES] [time_warp]   "
+        echo "  --help, -h           Show this help message            " 
+        exit 0;
     elif [ "${ARGI}" = "--nogui" ] ; then
-	GUI="no"
+        GUI="no"
     elif [ "${ARGI//[^0-9]/}" = "$ARGI" -a "$TIME_WARP" = 1 ]; then 
         TIME_WARP=$ARGI
     else 
@@ -34,17 +34,39 @@ done
 #----------------------------------------------------------
 #  Part 3: Launch the processes
 #----------------------------------------------------------
+
+# Find all vehicle_* files in the current directory
+vehicle_files=(vehicle_*.moos)
+
+# Get the total number of vehicle files found
+m=${#vehicle_files[@]}
+
+if [ $m -eq 0 ]; then
+    echo "No vehicle_*.moos files found. Exiting."
+    exit 1
+fi
+
 echo "Launching $COMMUNITY MOOS Community with WARP:" $TIME_WARP
+
+# Launch the main community process
 pAntler $COMMUNITY.moos --MOOSTimeWarp=$TIME_WARP >& /dev/null &
-for i in {seq 0 to "$m"}; do
-pAntler vehicle_"$n".moos --MOOSTimeWarp=$TIME_WARP >& /dev/null &
-((n++))
+
+# Launch all vehicle_*.moos files
+for vehicle in "${vehicle_files[@]}"; do
+    pAntler $vehicle --MOOSTimeWarp=$TIME_WARP >& /dev/null &
 done
+
+# Launch the shoreside process
 pAntler shoreside.moos --MOOSTimeWarp=$TIME_WARP >& /dev/null &
+
+# Launch uMAC for shoreside and all vehicles
 uMAC -t shoreside.moos &
-for i in {seq 0 to "$m"}; do
-uMAC -t vehicle_"$n".moos &
-((n++))
+for vehicle in "${vehicle_files[@]}"; do
+    uMAC -t $vehicle &
 done
+
+# Launch uMAC for the main community
 uMAC -t $COMMUNITY.moos
+
+# Kill all processes after completion
 kill -- -$$
