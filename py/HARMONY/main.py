ff59a0967acfcae281a7684938e461c7f30e04b2
Nathan Pedder <npedder2021@gmail.com>
2024-11-30 15:35:40 -0500

    Check added so UAV survey not displayed if no UAVs.

--- py/HARMONY/main.py
+++ py/HARMONY/main.py
@@ -31,7 +31,8 @@ def main():
 
         if moos_handler.survey_area is not None:
             moos_handler.notify("VIEW_GRID", moos_handler.survey_area.areaToGrid("Survey Area UUV"));
-            moos_handler.notify("VIEW_GRID", moos_handler.survey_area_land.areaToGrid("Survey Area UAV"));
+            if moos_handler.available_uavs:
+                moos_handler.notify("VIEW_GRID", moos_handler.survey_area_land.areaToGrid("Survey Area UAV"));
             # print(moos_handler.survey_area.areaToGrid())
             moos_handler.assign_and_notify()
             moos_handler.survey_area = None  # Reset survey area for the next iteration
